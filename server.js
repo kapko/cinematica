df8b77ee6e6da3f351d08fe89e5638132f854b1f
kzhekshen <kzhekshen@incentient.com>
2018-11-23 21:42:12 +0600

    fixed server

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.use(express.static(__dirname + '/src/'));
-// app.use(express.static('/'));
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/cinematica/'));
     app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/cinematica/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
